ee1f2e04a6fae439eddca1acc70d76c1fdb14e28
Xingchen Yang <xingchen.yang@mycit.ie>
2023-10-22 22:49:15 +0100

    add back botton in RegisterActivity

--- app/src/main/java/com/example/healthai/RegisterActivity.java
+++ app/src/main/java/com/example/healthai/RegisterActivity.java
@@ -1,12 +1,9 @@
 package com.example.healthai;
 
-
-
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 import androidx.annotation.NonNull;
@@ -20,9 +17,10 @@ import com.google.firebase.auth.FirebaseUser;
 public class RegisterActivity extends AppCompatActivity {
     private EditText usernameEditText;
     private EditText passwordEditText;
-
     private EditText confirmPasswordEditText;
     private Button RegisterButton;
+    private Button BackButton;  // Initialize BackButton
+
     private FirebaseAuth mAuth;
 
     @Override
@@ -34,9 +32,17 @@ public class RegisterActivity extends AppCompatActivity {
         passwordEditText = findViewById(R.id.passwordEditText);
         confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
         RegisterButton = findViewById(R.id.RegisterButton);
+        BackButton = findViewById(R.id.BackButton);
 
         // Initialize Firebase Authentication
         mAuth = FirebaseAuth.getInstance();
+
+        BackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();  // Finish the activity to navigate back
+            }
+        });
 
         RegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,35 +57,36 @@ public class RegisterActivity extends AppCompatActivity {
         String password = passwordEditText.getText().toString();
         String confirmPassword = confirmPasswordEditText.getText().toString();
 
-        if(!password.equals(confirmPassword)){
-            Toast.makeText(RegisterActivity.this, "Password does not match", Toast.LENGTH_SHORT).show();
+        if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            // Show an error message when either email or password is empty
+            Toast.makeText(RegisterActivity.this, "Please fill in all fields.", Toast.LENGTH_SHORT).show();
+            return;
         }
 
-        else{
-
+        if (!password.equals(confirmPassword)) {
+            Toast.makeText(RegisterActivity.this, "Password does not match", Toast.LENGTH_SHORT).show();
+        } else {
             mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Register successful
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            if (user != null) {
-                                // Redirect to the Register screen (RegisterActivity)
-                                Intent intent = new Intent(RegisterActivity.this, RegisterActivity.class);
-                                startActivity(intent);
-                                finish();
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Registration successful
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                if (user != null) {
+                                    // Redirect to the main screen (MainActivity)
+                                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
                             } else {
-                                // Handle the case where the user is null
+                                // Registration failed
+                                // Display an error message to the user
+                                Toast.makeText(RegisterActivity.this, "Registration failed. Check your credentials.", Toast.LENGTH_SHORT).show();
                             }
-                        } else {
-                            // Register failed
-                            // Display an error message to the user
-                            Toast.makeText(RegisterActivity.this, "Register failed. Check your credentials.", Toast.LENGTH_SHORT).show();
                         }
-                    }
-                });
+
+                    });
         }
     }
-
 }
