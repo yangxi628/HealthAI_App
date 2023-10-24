1b72f31381f8b16e81a4adb454956393fc58bd8c
Xingchen Yang <xingchen.yang@mycit.ie>
2023-10-24 20:40:55 +0100

    build all pages with four buttons working

--- app/src/main/java/com/example/healthai/AppointmentActivity.java
+++ app/src/main/java/com/example/healthai/AppointmentActivity.java
@@ -1,14 +1,56 @@
 package com.example.healthai;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AppointmentActivity extends AppCompatActivity {
+
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.appointment);
+        Button homeButton = findViewById(R.id.homeButton);
+        Button aiChatButton = findViewById(R.id.aiChatButton);
+        Button contactButton = findViewById(R.id.contactButton);
+        Button profileButton = findViewById(R.id.profileButton);
+
+
+
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.homeButton) {
+                    switchToActivity(HomeActivity.class);
+                } else if (v.getId() == R.id.aiChatButton) {
+                    switchToActivity(AiChatActivity.class);
+                } else if (v.getId() == R.id.contactButton) {
+                    switchToActivity(ContactActivity.class);
+                } else if (v.getId() == R.id.profileButton) {
+                    switchToActivity(ProfileActivity.class);
+                }
+            }
+        };
+
+        // Assign the common click listener to all buttons
+        homeButton.setOnClickListener(buttonClickListener);
+        aiChatButton.setOnClickListener(buttonClickListener);
+        contactButton.setOnClickListener(buttonClickListener);
+        profileButton.setOnClickListener(buttonClickListener);
+
+
+
     }
+
+    private void switchToActivity(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/example/healthai/RatingActivity.java
+++ app/src/main/java/com/example/healthai/RatingActivity.java
@@ -1,6 +1,9 @@
 package com.example.healthai;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +13,40 @@ public class RatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rating);
+        Button homeButton = findViewById(R.id.homeButton);
+        Button aiChatButton = findViewById(R.id.aiChatButton);
+        Button contactButton = findViewById(R.id.contactButton);
+        Button profileButton = findViewById(R.id.profileButton);
+
+
+
+        View.OnClickListener buttonClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.homeButton) {
+                    switchToActivity(HomeActivity.class);
+                } else if (v.getId() == R.id.aiChatButton) {
+                    switchToActivity(AiChatActivity.class);
+                } else if (v.getId() == R.id.contactButton) {
+                    switchToActivity(ContactActivity.class);
+                } else if (v.getId() == R.id.profileButton) {
+                    switchToActivity(ProfileActivity.class);
+                }
+            }
+        };
+
+        // Assign the common click listener to all buttons
+        homeButton.setOnClickListener(buttonClickListener);
+        aiChatButton.setOnClickListener(buttonClickListener);
+        contactButton.setOnClickListener(buttonClickListener);
+        profileButton.setOnClickListener(buttonClickListener);
+    }
+
+
+
+    private void switchToActivity(Class<?> cls) {
+        Intent intent = new Intent(this, cls);
+        startActivity(intent);
     }
 }
 // luke
