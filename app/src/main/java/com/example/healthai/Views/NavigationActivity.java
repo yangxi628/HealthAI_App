ae46073b6911a3016fc61b041c95f83e292f87f8
lkiely1 <lkiely1@mycit.ie>
2023-12-03 20:14:14 +0000

    made it so doctors/ users who are not patients cannot use the support function (prevents crash)

--- app/src/main/java/com/example/healthai/Views/NavigationActivity.java
+++ app/src/main/java/com/example/healthai/Views/NavigationActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,13 @@ import androidx.fragment.app.FragmentTransaction;
 
 import com.example.healthai.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
 public class NavigationActivity extends AppCompatActivity {
     FrameLayout frameLayout;
     BottomNavigationView bottomNavigationView;
@@ -36,7 +44,30 @@ public class NavigationActivity extends AppCompatActivity {
                 } else if (itemId == R.id.aichat) {
                     replaceFragment(new AIChatFragment());
                 } else if (itemId == R.id.support) {
-                    replaceFragment(new SupportFragment());
+                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                    String userid = user.getUid(); // Get the UID of the currently signed-in user
+                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                    DocumentReference userDocRef = db.collection("users").document(userid); // Reference to the user's document
+                    userDocRef.get().addOnSuccessListener(documentSnapshot -> {
+                        if (documentSnapshot.exists()) {
+                            String role = documentSnapshot.getString("role");
+                            if (Objects.equals(role, "patient")) {
+                                replaceFragment(new SupportFragment());
+
+                            }
+                            else if (!Objects.equals(role, "patient")) {
+                                Toast.makeText(NavigationActivity.this, "Doctor cannot use this", Toast.LENGTH_SHORT).show();
+                                Toast.makeText(NavigationActivity.this, role, Toast.LENGTH_SHORT).show();
+                            }
+
+                        } else {
+                            Toast.makeText(NavigationActivity.this, "Profile doesn't exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(e -> {
+                        Toast.makeText(NavigationActivity.this, "access failed", Toast.LENGTH_SHORT).show();
+                    });
+
+
                 }
 
                 return true;
